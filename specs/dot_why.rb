6d6185195d5faf6caa2de90b59aba3f4f71af42a
da99 <i-hate-spam-1234567@mailinator.com>
2014-02-03 15:35:42 -0600

    Dev. checkpoint.

--- specs/dot_why.rb
+++ specs/dot_why.rb
@@ -1,12 +1,32 @@
+require "dot_why"
 
-describe "partial" do
+describe "partial()" do
+
+  before do
+    @file = "/tmp/" + rand(1000).to_s + '.dot_why.spec.rb'
+    File.open(@file, 'w') { |file|
+      file.write %~
+      title "test"
+      ~
+    }
+    @page = Dot_Why::Template.new(@file)
+    def @page.main
+      eval_main
+    end
+  end
+
+  after do
+    `rm #{@file}` if File.exists?(@file)
+  end
 
   it "grabs and evals specified file" do
-    
+    @page.to_html(:prettyprint=>true)
+    .should == "test"
   end
 
   it "grabs/evals 2nd file based on dir of first arg." do
-    
+    @page.to_html(:prettyprint=>true)
+    .should == "partial"
   end
 
 end # === describe partial ===
---
+++ specs/files/layout.rb
@@ -0,0 +1,8 @@
+
+module Dot_Why
+  class Template
+
+    def_sections :styles, :js_templates
+
+  end
+end # === module
